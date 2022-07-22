548f50a0f59fce8cbd784a7aff451d71b73b132d
Ranjan <k.rajan9572@gmail.com>
2022-07-22 07:35:56 +0530

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-// Write your code here:
-var student = {
-	name : "Ranjan",
-	age: 25,
-	rollno : 7,
-};
+/*Write a program to print 1 after 1 sec, 2 after 2 sec till 5. Complete the given code to get the desired output as shown in the expected output.
+Expected Output*/
 
-student.address = {
-    City : "sasaram",
-	State: "bihar"
+
+
+
+
+
+function timer() {
+    for(let i = 1; i<=5; i++){
+        setTimeout(function() {
+             
+        console.log(i)
+    },1000);
+         
+          
+        
+    }
+    console.log("Hello ninjas!");
 }
-
-//console.log(student);
-delete student.rollno;
-console.log(student);
+timer();
